8fe6230ea4226937452d1df6ded4ba60779a2532
09740944710 <elcio.paula@serpro.gov.br>
2021-12-31 19:58:21 -0300

    início do módulo aquisições

---
+++ application/controllers/Aquisicoes.php
@@ -0,0 +1,208 @@
+<?php if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+class Aquisicoes extends MY_Controller
+{
+
+    /**
+     * author: Ramon Silva
+     * email: silva018-mg@yahoo.com.br
+     *
+     */
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->helper('form');
+        $this->load->model('aquisicoes_model');
+        $this->data['menuAquisicoes'] = 'Aquisicoes';
+    }
+
+    public function index()
+    {
+        $this->gerenciar();
+    }
+
+    public function gerenciar()
+    {
+        if (!$this->permission->checkPermission($this->session->userdata('permissao'), 'vAquisicao')) {
+            $this->session->set_flashdata('error', 'Você não tem permissão para visualizar aquisicoes.');
+            redirect(base_url());
+        }
+
+        $this->load->library('pagination');
+
+        $this->data['configuration']['base_url'] = site_url('aquisicoes/gerenciar/');
+        $this->data['configuration']['total_rows'] = $this->aquisicoes_model->count('aquisicoes');
+
+        $this->pagination->initialize($this->data['configuration']);
+
+        $this->data['results'] = $this->aquisicoes_model->get('aquisicoes', '*', '', $this->data['configuration']['per_page'], $this->uri->segment(3));
+
+        $this->data['view'] = 'aquisicoes/aquisicoes';
+        return $this->layout();
+    }
+
+    public function adicionar()
+    {
+        if (!$this->permission->checkPermission($this->session->userdata('permissao'), 'aProduto')) {
+            $this->session->set_flashdata('error', 'Você não tem permissão para adicionar aquisicoes.');
+            redirect(base_url());
+        }
+
+        $this->load->library('form_validation');
+        $this->data['custom_error'] = '';
+
+        if ($this->form_validation->run('aquisicoes') == false) {
+            $this->data['custom_error'] = (validation_errors() ? '<div class="form_error">' . validation_errors() . '</div>' : false);
+        } else {
+            $precoCompra = $this->input->post('precoCompra');
+            $precoCompra = str_replace(",", "", $precoCompra);
+            $precoVenda = $this->input->post('precoVenda');
+            $precoVenda = str_replace(",", "", $precoVenda);
+            $data = [
+                'codDeBarra' => set_value('codDeBarra'),
+                'descricao' => set_value('descricao'),
+                'unidade' => set_value('unidade'),
+                'precoCompra' => $precoCompra,
+                'precoVenda' => $precoVenda,
+                'estoque' => set_value('estoque'),
+                'estoqueMinimo' => set_value('estoqueMinimo'),
+                'saida' => set_value('saida'),
+                'entrada' => set_value('entrada'),
+            ];
+
+            if ($this->aquisicoes_model->add('aquisicoes', $data) == true) {
+                $this->session->set_flashdata('success', 'Produto adicionado com sucesso!');
+                log_info('Adicionou um produto');
+                redirect(site_url('aquisicoes/adicionar/'));
+            } else {
+                $this->data['custom_error'] = '<div class="form_error"><p>An Error Occured.</p></div>';
+            }
+        }
+        $this->data['view'] = 'aquisicoes/adicionarProduto';
+        return $this->layout();
+    }
+
+    public function editar()
+    {
+        if (!$this->uri->segment(3) || !is_numeric($this->uri->segment(3))) {
+            $this->session->set_flashdata('error', 'Item não pode ser encontrado, parâmetro não foi passado corretamente.');
+            redirect('mapos');
+        }
+
+        if (!$this->permission->checkPermission($this->session->userdata('permissao'), 'eProduto')) {
+            $this->session->set_flashdata('error', 'Você não tem permissão para editar aquisicoes.');
+            redirect(base_url());
+        }
+        $this->load->library('form_validation');
+        $this->data['custom_error'] = '';
+
+        if ($this->form_validation->run('aquisicoes') == false) {
+            $this->data['custom_error'] = (validation_errors() ? '<div class="form_error">' . validation_errors() . '</div>' : false);
+        } else {
+            $precoCompra = $this->input->post('precoCompra');
+            $precoCompra = str_replace(",", "", $precoCompra);
+            $precoVenda = $this->input->post('precoVenda');
+            $precoVenda = str_replace(",", "", $precoVenda);
+            $data = [
+                'codDeBarra' => set_value('codDeBarra'),
+                'descricao' => $this->input->post('descricao'),
+                'unidade' => $this->input->post('unidade'),
+                'precoCompra' => $precoCompra,
+                'precoVenda' => $precoVenda,
+                'estoque' => $this->input->post('estoque'),
+                'estoqueMinimo' => $this->input->post('estoqueMinimo'),
+                'saida' => set_value('saida'),
+                'entrada' => set_value('entrada'),
+            ];
+
+            if ($this->aquisicoes_model->edit('aquisicoes', $data, 'idAquisicoes', $this->input->post('idAquisicoes')) == true) {
+                $this->session->set_flashdata('success', 'Produto editado com sucesso!');
+                log_info('Alterou um produto. ID: ' . $this->input->post('idAquisicoes'));
+                redirect(site_url('aquisicoes/editar/') . $this->input->post('idAquisicoes'));
+            } else {
+                $this->data['custom_error'] = '<div class="form_error"><p>An Error Occured</p></div>';
+            }
+        }
+
+        $this->data['result'] = $this->aquisicoes_model->getById($this->uri->segment(3));
+
+        $this->data['view'] = 'aquisicoes/editarProduto';
+        return $this->layout();
+    }
+
+    public function visualizar()
+    {
+        if (!$this->uri->segment(3) || !is_numeric($this->uri->segment(3))) {
+            $this->session->set_flashdata('error', 'Item não pode ser encontrado, parâmetro não foi passado corretamente.');
+            redirect('mapos');
+        }
+
+        if (!$this->permission->checkPermission($this->session->userdata('permissao'), 'vProduto')) {
+            $this->session->set_flashdata('error', 'Você não tem permissão para visualizar aquisicoes.');
+            redirect(base_url());
+        }
+
+        $this->data['result'] = $this->aquisicoes_model->getById($this->uri->segment(3));
+
+        if ($this->data['result'] == null) {
+            $this->session->set_flashdata('error', 'Produto não encontrado.');
+            redirect(site_url('aquisicoes/editar/') . $this->input->post('idAquisicoes'));
+        }
+
+        $this->data['view'] = 'aquisicoes/visualizarProduto';
+        return $this->layout();
+    }
+
+    public function excluir()
+    {
+        if (!$this->permission->checkPermission($this->session->userdata('permissao'), 'dProduto')) {
+            $this->session->set_flashdata('error', 'Você não tem permissão para excluir aquisicoes.');
+            redirect(base_url());
+        }
+
+        $id = $this->input->post('id');
+        if ($id == null) {
+            $this->session->set_flashdata('error', 'Erro ao tentar excluir produto.');
+            redirect(base_url() . 'index.php/aquisicoes/gerenciar/');
+        }
+
+        $this->aquisicoes_model->delete('aquisicoes_os', 'aquisicoes_id', $id);
+        $this->aquisicoes_model->delete('itens_de_vendas', 'aquisicoes_id', $id);
+        $this->aquisicoes_model->delete('aquisicoes', 'idAquisicoes', $id);
+
+        log_info('Removeu um produto. ID: ' . $id);
+
+        $this->session->set_flashdata('success', 'Produto excluido com sucesso!');
+        redirect(site_url('aquisicoes/gerenciar/'));
+    }
+
+    public function atualizar_estoque()
+    {
+        if (!$this->permission->checkPermission($this->session->userdata('permissao'), 'eProduto')) {
+            $this->session->set_flashdata('error', 'Você não tem permissão para atualizar estoque de aquisicoes.');
+            redirect(base_url());
+        }
+
+        $idProduto = $this->input->post('id');
+        $novoEstoque = $this->input->post('estoque');
+        $estoqueAtual = $this->input->post('estoqueAtual');
+
+        $estoque = $estoqueAtual + $novoEstoque;
+
+        $data = [
+            'estoque' => $estoque,
+        ];
+
+        if ($this->aquisicoes_model->edit('aquisicoes', $data, 'idAquisicoes', $idProduto) == true) {
+            $this->session->set_flashdata('success', 'Estoque de Produto atualizado com sucesso!');
+            log_info('Atualizou estoque de um produto. ID: ' . $idProduto);
+            redirect(site_url('aquisicoes/visualizar/') . $idProduto);
+        } else {
+            $this->data['custom_error'] = '<div class="alert">Ocorreu um erro.</div>';
+        }
+    }
+}
---
+++ application/models/Aquisicoes_model.php
@@ -0,0 +1,83 @@
+<?php
+class Aquisicoes_model extends CI_Model
+{
+
+    /**
+     * author: Elcio Silva
+     * email: elciospy@gmail.com
+     *
+     */
+    
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    
+    public function get($table, $fields, $where = '', $perpage = 0, $start = 0, $one = false, $array = 'array')
+    {
+        $this->db->select($fields);
+        $this->db->from($table);
+        $this->db->order_by('idAquisicoes', 'desc');
+        $this->db->limit($perpage, $start);
+        if ($where) {
+            $this->db->where($where);
+        }
+        
+        $query = $this->db->get();
+        
+        $result =  !$one  ? $query->result() : $query->row();
+        return $result;
+    }
+
+    public function getById($id)
+    {
+        $this->db->where('idAquisicoes', $id);
+        $this->db->limit(1);
+        return $this->db->get('aquisicoes')->row();
+    }
+    
+    public function add($table, $data)
+    {
+        $this->db->insert($table, $data);
+        if ($this->db->affected_rows() == '1') {
+            return true;
+        }
+        
+        return false;
+    }
+    
+    public function edit($table, $data, $fieldID, $ID)
+    {
+        $this->db->where($fieldID, $ID);
+        $this->db->update($table, $data);
+
+        if ($this->db->affected_rows() >= 0) {
+            return true;
+        }
+        
+        return false;
+    }
+    
+    public function delete($table, $fieldID, $ID)
+    {
+        $this->db->where($fieldID, $ID);
+        $this->db->delete($table);
+        if ($this->db->affected_rows() == '1') {
+            return true;
+        }
+        
+        return false;
+    }
+    
+    public function count($table)
+    {
+        return $this->db->count_all($table);
+    }
+
+    public function updateEstoque($produto, $quantidade, $operacao = '-')
+    {
+        $sql = "UPDATE aquisicao set estoque = estoque $operacao ? WHERE idAquisicoes = ?";
+        return $this->db->query($sql, [$quantidade, $produto]);
+    }
+}
